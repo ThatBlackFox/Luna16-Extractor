062dcb34ca14de7a3c542f0b15e703acd4573f68
Shaiz Ahmed Shariff <shaizahmed.shariff5@gmail.com>
2025-03-14 16:05:22 +0530

    removes masks of empty scans

--- utils/patching.py
+++ utils/patching.py
@@ -69,6 +69,7 @@ def patching(data: dict):
                 print(f"Warning: Node - {index} @ {parent} failed to patch")
         if empty:
             data[parent] = True
+            continue
         out_path = os.path.join(OUTPUT_DIR, parent)
         sitk.WriteImage(blank_image, f"{out_path}_seg.mhd")
     with open(os.path.join(OUTPUT_DIR, "meta.json"), 'w') as f:
