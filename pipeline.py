ca55563e44d0f6e1504dd72f2397047847a65002
Amogha Y A <108296426+ThatBlackFox@users.noreply.github.com>
2025-03-25 11:09:14 +0530

    Add pipeline script and fix help msg in drr

---
+++ pipeline.py
@@ -0,0 +1,49 @@
+import subprocess
+import sys
+import os
+
+
+args = sys.argv
+MAIN_DATA_DIR = args[args.index('-d')+1]
+MAIN_OUTPUT_DIR = args[args.index('-o')+1]
+CSV_PATH = args[args.index('-c')+1]
+PATCH_MASK_DIR = os.path.join(MAIN_OUTPUT_DIR, 'patch_dataset')
+INFERENCE_DIR = os.path.join(MAIN_OUTPUT_DIR, 'infered_dataset')
+FULL_MASK_DIR = os.path.join(MAIN_OUTPUT_DIR, 'full_mask_dataset')
+META_PATH = os.path.join(PATCH_MASK_DIR, 'meta.json')
+XRAY_PATH = os.path.join(MAIN_OUTPUT_DIR, 'xray_dataset')
+
+extractor = f"python dataset_maker.py -d {MAIN_DATA_DIR} -o {PATCH_MASK_DIR} -c {CSV_PATH}"
+infer = f"python dataset_maker.py -d {PATCH_MASK_DIR} -o {INFERENCE_DIR}"
+patcher = f"python dataset_patcher.py -d {MAIN_DATA_DIR} -o {FULL_MASK_DIR} -r {INFERENCE_DIR} -m {META_PATH}"
+drrer = f"python drrer.py -d {MAIN_DATA_DIR} -m {FULL_MASK_DIR} -o {XRAY_PATH}"
+
+print("Starting Pipeline")
+print("Starting Extraction")
+
+process = subprocess.Popen(extractor)
+process.wait()
+
+print("Finished Extraction")
+print("Starting Inference")
+
+process = subprocess.Popen(infer)
+process.wait()  
+
+print("Finished Inference")
+print("Starting Patching")
+
+process = subprocess.Popen(patcher)
+process.wait()
+
+print("Finished Patching")
+print("Starting DRR")
+
+process = subprocess.Popen(drrer)
+process.wait()  
+
+print("Finished DRR")
+print("Pipeline execution completed.")
+
+
+
--- utils/cli_drr_handler.py
+++ utils/cli_drr_handler.py
@@ -12,28 +12,28 @@ def check_args(args: list):
             raise ValueError(f"{arg} is a mandatory keyword, use -h for help")
 
 def print_help():
-    help_message = """\033[1;36m
-CT Patch Extractor - Help
+    help_message = """
+\033[1;36m
+DRR Maker - Help
 \033[0m
 Usage:
-  python dataset_maker.py -d <data_directory> -o <output_directory> [-c <csv_file>]
+  python drr_maker.py -d <data_directory> -m <mask_directory> -o <output_directory>
 
 \033[1;33mOptions:\033[0m
   \033[1;32m-d\033[0m    Path to the directory containing full chest CT scans (.mhd)  \033[1;30m[Required]\033[0m
-  \033[1;32m-o\033[0m    Output directory to store extracted CT patches                \033[1;30m[Required]\033[0m
-  \033[1;32m-c\033[0m    Path to the CSV file containing annotations (optional)
+  \033[1;32m-m\033[0m    Path to the directory containing corresponding masks (.mhd)  \033[1;30m[Required]\033[0m
+  \033[1;32m-o\033[0m    Output directory to store generated DRRs                 \033[1;30m[Required]\033[0m
 
 \033[1;33mExample Usage:\033[0m
-  \033[1;34mpython script.py -d /path/to/ct_scans -o /path/to/output\033[0m
-  \033[1;34mpython script.py -d /path/to/ct_scans -o /path/to/output -c annotations.csv\033[0m
+  \033[1;34mpython drr_maker.py -d /path/to/ct_scans -m /path/to/masks -o /path/to/output\033[0m
 
 \033[1;35mDescription:\033[0m
-  This script extracts patches from chest CT scans stored in the specified directory.
-  If an annotation CSV file is provided, patches will be extracted based on its coordinates.
-  Else the script expects an annotation CSV file in the data directory.
+  This script generates Digitally Reconstructed Radiographs (DRRs) from chest CT scans.
+  It requires both the CT scan directory and the corresponding mask directory.
     """
     print(help_message)
-    
+
+
 def check_help(args: list):
     if '-h' in args:
         print_help()
