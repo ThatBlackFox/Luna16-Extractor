339f3df34dcd7f8b7ea507c776a8d854e5c0a2a3
Amogha Y A <108296426+ThatBlackFox@users.noreply.github.com>
2025-03-26 11:35:22 +0530

    FIx pathing issues

--- pipeline.py
+++ pipeline.py
@@ -10,7 +10,8 @@ CSV_PATH = args[args.index('-c')+1]
 PATCH_MASK_DIR = os.path.join(MAIN_OUTPUT_DIR, 'patch_dataset')
 INFERENCE_DIR = os.path.join(MAIN_OUTPUT_DIR, 'infered_dataset')
 FULL_MASK_DIR = os.path.join(MAIN_OUTPUT_DIR, 'full_mask_dataset')
-META_PATH = os.path.join(PATCH_MASK_DIR, 'meta.json')
+META_PATCH_DATASET_PATH = os.path.join(PATCH_MASK_DIR, 'meta.json')
+META_FULL_MASK_PATH  = os.path.join(FULL_MASK_DIR, 'meta.json')
 XRAY_DIR = os.path.join(MAIN_OUTPUT_DIR, 'xray_dataset')
 
 paths = [PATCH_MASK_DIR, INFERENCE_DIR, FULL_MASK_DIR, XRAY_DIR]
@@ -19,8 +20,8 @@ for path in paths:
 
 extractor = f"python dataset_maker.py -d {MAIN_DATA_DIR} -o {PATCH_MASK_DIR} -c {CSV_PATH}"
 infer = f"python data_inference_vnet.py -i {PATCH_MASK_DIR} -o {INFERENCE_DIR}"
-patcher = f"python dataset_patcher.py -d {MAIN_DATA_DIR} -o {FULL_MASK_DIR} -r {INFERENCE_DIR} -m {META_PATH}"
-drrer = f"python drrer.py -d {MAIN_DATA_DIR} -m {FULL_MASK_DIR} -o {XRAY_DIR} --meta {META_PATH}"
+patcher = f"python dataset_patcher.py -d {MAIN_DATA_DIR} -o {FULL_MASK_DIR} -r {INFERENCE_DIR} -m {META_PATCH_DATASET_PATH}"
+drrer = f"python drrer.py -d {MAIN_DATA_DIR} -m {FULL_MASK_DIR} -o {XRAY_DIR} --meta {META_FULL_MASK_PATH}"
 
 print("Starting Pipeline")
 print("Starting Extraction")
